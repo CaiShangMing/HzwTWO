543a13a9b047bdfabecfdd729d20027de8e10d9e
LY-Ming <35759344+CaiShangMing@users.noreply.github.com>
2019-04-17 12:21:40 +0800

    Revert "firstEdition"
    This reverts commit 682cc2d682dcc2699bac9722f243e7e460662eab.

--- hzw4/WebRoot/WEB-INF/jsp/common/main.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
-
-<%@include file="/WEB-INF/jsp/common/head.jsp"%>
-
-<%@include file="/WEB-INF/jsp/common/foot.jsp"%>
--- hzw4/WebRoot/statics/localjs/homePage.js
+++
@@ -1,8 +0,0 @@
-/*$(document).ready(function(){
-	$(".personInformation").mouseenter(function(){
-		$(this).addClass("btn-information");
-	});
-	$(".personInformation").mouseleave(function(){
-		$(".personInformation").removeClass("btn-information");
-	});
-});*/
